c46f9397ce0c97c399452069859dff1fbab63a7f
Kenny Lee <kenny.lee@appdetex.com>
2019-10-30 11:19:27 -0600

    improves exposures

--- src/components/ExposureMachine.vue
+++ src/components/ExposureMachine.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="parse">
+  <div class="parse col-md-12">
     <h2>Exposure</h2>
+    <span>
+      <b-button
+        :variant="theme"
+        download
+        class="badge badge-info"
+        @click="savePlayersList"
+      >
+        Download This Player List
+      </b-button>
+    </span>
     <div>
       <!-- Styled -->
       <b-form-file
@@ -15,67 +25,146 @@
         Selected file: {{ file ? file.name : '' }}
       </div>
     </div>
-    <div class="body col-md-12">
-      <b-table
-        v-if="playersList"
-        striped
-        hover
-        :items="getUnique(playersList, 'ID')"
-        :fields="playersListFields"
-        style="font-size: small;"
-      >
-        <template
-          slot="delete_btn"
-          slot-scope="row"
+    <div class="body col-md-10">
+      <b-tabs v-if="playersList">
+        <b-tab
+          title="All Players"
+          active
         >
-          <b-button
-            size="sm"
-            class="badge badge-primary"
-            @click.stop="addToPlayerPool(playersList[row.index])"
+          <b-table
+            v-if="playersList"
+            striped
+            hover
+            :items="getUnique(playersList, 'ID')"
+            :fields="playersListFields"
+            style="font-size: small;"
+            class="col-md-5"
           >
-            Add to Pool
-          </b-button>
-        </template>
-      </b-table>
-      <span>
-        <b-button
-          :variant="theme"
-          download
-          class="badge badge-info"
-          @click="savePlayersList"
-        >
-          Download This Player List
-        </b-button>
-      </span>
-      <span class="col-md-6 float-right">
-        <b-table
-          striped
-          hover
-          :items="getUnique(playerPool.players, 'ID')"
-          :fields="playersListFields"
-          style="font-size: small;"
-        >
-          <template
-            slot="delete_btn"
-            slot-scope="row"
+            <template
+              slot="delete_btn"
+              slot-scope="row"
+            >
+              <b-button
+                size="sm"
+                class="badge badge-primary"
+                @click.stop="addToPlayerPool(playersList[row.index])"
+              >
+                Add to Pool
+              </b-button>
+            </template>
+          </b-table>
+        </b-tab>
+        <b-tab title="Players By Team">
+          <div class="alert alert-light">
+            <b-btn
+              v-for="team in Object.keys(teams).sort()"
+              :key="team.Team"
+              variant="link"
+              size="sm"
+              @click="setTeam(team)"
+            >
+              {{ team }}
+            </b-btn>
+          </div>
+          <b-table
+            v-if="team && team.length"
+            striped
+            hover
+            :items="team"
+            :fields="playersListFields"
           >
-            <b-button
-              size="sm"
-              class="badge badge-danger"
-              @click.stop="removePlayer(playerPool.players[row.index])"
+            <template
+              slot="delete_btn"
+              slot-scope="row"
             >
-              Remove
-            </b-button>
-            <br>            
-            <b-button
+              <b-button
+                size="sm"
+                class="badge badge-primary"
+                @click.stop="addToPlayerPool(team[row.index])"
+              >
+                Add to Pool
+              </b-button>
+            </template>
+          </b-table>
+        </b-tab>
+        <b-tab title="Players By Position">
+          <div class="alert alert-light">
+            <b-btn
+              v-for="position in Object.keys(positions)"
+              :key="position.Position"
+              variant="link"
               size="sm"
-              class="badge badge-success"
-              @click.stop="addPlayer(playerPool.players[row.index])"
+              @click="setPosition(position)"
             >
-              Increase Exposure
-            </b-button>
-          </template>
-        </b-table>
+              {{ position }}
+            </b-btn>
+          </div>
+          <b-table
+            v-if="position && position.length"
+            striped
+            hover
+            :items="position"
+            :fields="playersListFields"
+          >
+            <template
+              slot="delete_btn"
+              slot-scope="row"
+            >
+              <b-button
+                size="sm"
+                class="badge badge-primary"
+                @click.stop="addToPlayerPool(position[row.index])"
+              >
+                Add to Pool
+              </b-button>
+            </template>
+          </b-table>
+        </b-tab>
+      </b-tabs>
+      <span class="col-md-5 float-right">
+        <b-tabs v-if="playersList">
+          <b-tab title="All Players">
+            <div
+              v-if="playersList"
+              class="alert alert-light"
+            >
+              <b-btn
+                variant="link"
+                size="sm"
+              >
+                Does nothing
+              </b-btn>
+            </div>
+            <b-table
+              striped
+              hover
+              :items="getUnique(playerPool.players, 'ID')"
+              :fields="playersListFields"
+              style="font-size: small;"
+            >
+              <template
+                slot="delete_btn"
+                slot-scope="row"
+              >
+                <b-button
+                  size="sm"
+                  class="badge badge-danger"
+                  @click.stop="removePlayer(playerPool.players[row.index])"
+                >
+                  Remove
+                </b-button>
+                <br>
+                <b-button
+                  size="sm"
+                  class="badge badge-success"
+                  @click.stop="addPlayer(playerPool.players[row.index])"
+                >
+                  Increase Exposure
+                </b-button>
+              </template>
+            </b-table>
+          </b-tab>
+        </b-tabs>
       </span>
     </div>
   </div>
@@ -105,7 +194,9 @@ export default {
         on: false
       },
       exposure: {},
-      playerPool: { players: [] }
+      playerPool: { players: [] },
+      currTeam: '',
+      currPosition: ''
     }
   },
   created () {
@@ -114,15 +205,30 @@ export default {
     });
   },
   methods: {
+    drawTeams () {
+      this.teams = this.groupBy(this.playersList, "TeamAbbrev");
+      delete this.teams["undefined"];
+    },
+    drawPositions () {
+      this.positions = this.groupBy(this.playersList, "Position");
+    },
     addToPlayerPool (player) {
       this.playerPool.players.push(player);
-      console.log(this.playerPool.players)
+      this.removePlayer(player);
     },
     removePlayer (player) {
       let id = player.ID
-      this.playerPool = this.playerPool.filter(function (player) {
+      this.playersList = this.playersList.filter(function (player) {
         return player.ID !== id;
       });
+      this.drawTeams();
+      this.drawPositions();
+      if (this.currTeam) {
+        this.setTeam(this.currTeam);
+      }
+      if (this.currPosition) {
+        this.setPosition(this.currPosition);
+      }
     },
     addPlayer (player) {
       this.playerPool.players.push(player);
@@ -142,6 +248,8 @@ export default {
                 sortable: true
               }
             });
+            that.drawTeams();
+            that.drawPositions();
             that.playersListFields.push('delete_btn');
           },
           error (errors) {
@@ -190,6 +298,14 @@ export default {
         arr[x[property]].push(x);
         return arr;
       }, {});
+    },
+    setTeam (team) {
+      this.team = this.teams[team];
+      this.currTeam = team;
+    },
+    setPosition (pos) {
+      this.position = this.positions[pos];
+      this.currPosition = pos;
     }
   }
 }
@@ -223,5 +339,10 @@ export default {
 
 .generate-progress-bar {
   margin-top: 10px !important;
+}
+.alert-light{
+  min-height:8vh;
+  max-height: 8vh;
+  overflow-y:scroll;
 }
 </style>
